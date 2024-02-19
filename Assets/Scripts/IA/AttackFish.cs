daa9cd581fe6143d8028f0a0606425c014d37544
Luisiitt <144843710+Luisiitt@users.noreply.github.com>
2024-02-19 18:30:49 +0100

    IA + Pez

---
+++ Assets/Scripts/IA/AttackFish.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackFish : MonoBehaviour
+{
+
+    public int rutina;
+    public float contador;
+    //public Animator animator;
+    public Quaternion angulo;
+    public float grado;
+
+    public GameObject target;
+    public bool atacando=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  animator = GetComponent<Animator>();
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        comportamiento();
+    }
+
+    public void comportamiento()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > 5)
+        {
+            //animator.SetBool("run", false);
+
+
+            contador += 1 * Time.deltaTime;
+            if (contador >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                contador = 0;
+            }
+
+            switch (rutina)
+            {
+                case 0:
+                    //animator.SetBool("walk", false);
+                    break;
+                case 1: //determina direccion de desplazamiento
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    //animator.SetBool("walk", true);
+                    break;
+
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, target.transform.position) > 1 && !atacando)
+            {
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+                //animator.SetBool("walk", false);
+
+                //animator.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+                //animator.SetBool("attack", false);
+                atacando = false;
+
+            }
+            else
+            {
+                //animator.SetBool("walk", false);
+                //animator.SetBool("run", false);
+
+                //animator.SetBool("attack", true);
+                atacando = true;
+            }
+        }
+    }
+
+    public void FinalAtaque()
+    {
+        //animator.SetBool("attack", false);
+        atacando = false;
+
+    }
+
+
+}
